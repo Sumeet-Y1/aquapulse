7e838fd67d42167c0e0d16aaa74ef137cff6a182
Sumeet-Y1 <sumeety202@gmail.com>
2026-07-25 17:13:08 +0530

    Add SocietyResponse DTO

---
+++ src/main/java/com/aquapulse/backend/dto/SocietyResponse.java
@@ -0,0 +1,13 @@
+package com.aquapulse.backend.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class SocietyResponse {
+    private Long id;
+    private String name;
+    private String address;
+    private String city;
+}
