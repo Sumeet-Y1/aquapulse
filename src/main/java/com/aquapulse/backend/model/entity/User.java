ec8e6a09dab18641613c14ff1dd27439b0f55a95
Sumeet-Y1 <sumeety202@gmail.com>
2026-07-25 13:46:09 +0530

    Add User entity

---
+++ src/main/java/com/aquapulse/backend/model/entity/User.java
@@ -0,0 +1,39 @@
+package com.aquapulse.backend.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(nullable = false)
+    private String fullName;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @ManyToOne
+    @JoinColumn(name = "society_id")
+    private Society society;
+
+    public enum Role {
+        ADMIN, RESIDENT
+    }
+}
