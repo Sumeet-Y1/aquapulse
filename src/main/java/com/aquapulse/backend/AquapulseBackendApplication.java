ee56dd84a1cf17ac203efc813001ce9da6378a0b
Sumeet-Y1 <sumeety202@gmail.com>
2026-07-25 13:39:43 +0530

    Initial project structure with entities

---
+++ src/main/java/com/aquapulse/backend/AquapulseBackendApplication.java
@@ -0,0 +1,13 @@
+package com.aquapulse.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AquapulseBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AquapulseBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aquapulse/backend/AquapulseBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aquapulse.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AquapulseBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
