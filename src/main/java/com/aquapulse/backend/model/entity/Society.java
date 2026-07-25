739d777064c2856648061bcc2a75355bd9d38bc1
Sumeet-Y1 <sumeety202@gmail.com>
2026-07-25 13:46:15 +0530

    Add Society entity

---
+++ src/main/java/com/aquapulse/backend/model/entity/Society.java
@@ -0,0 +1,30 @@
+package com.aquapulse.backend.model.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+import java.util.List;
+
+@Entity
+@Table(name = "societies")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Society {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private String address;
+
+    private String city;
+
+    @OneToMany(mappedBy = "society", cascade = CascadeType.ALL)
+    private List<RWHUnit> units;
+}
