9afe5b0aa74ed562db2fea8b32cf018e5ae77f3f
Sumeet-Y1 <sumeety202@gmail.com>
2026-07-26 21:57:57 +0530

    Allow nullable password for Google-authenticated users

--- src/main/java/com/aquapulse/backend/model/entity/User.java
+++ src/main/java/com/aquapulse/backend/model/entity/User.java
@@ -19,7 +19,7 @@ public class User {
     @Column(nullable = false, unique = true)
     private String email;
 
-    @Column(nullable = false)
+    @Column
     private String password;
 
     @Column(nullable = false)
