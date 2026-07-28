7982b3f1ca9cd47f9c40c34a077fcaaaa9cee7ea
Sumeet-Y1 <sumeety202@gmail.com>
2026-07-28 16:20:18 +0530

    Add CompleteGoogleSignupRequest DTO

--- src/main/java/com/aquapulse/backend/dto/CompleteGoogleSignupRequest.java
+++ src/main/java/com/aquapulse/backend/dto/CompleteGoogleSignupRequest.java
@@ -1,4 +1,21 @@
 package com.aquapulse.backend.dto;
 
+import com.aquapulse.backend.model.entity.User;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import lombok.Data;
+
+@Data
 public class CompleteGoogleSignupRequest {
+
+    @Email
+    @NotBlank
+    private String email;
+
+    @NotBlank
+    private String fullName;
+
+    @NotNull
+    private User.Role role;
 }
