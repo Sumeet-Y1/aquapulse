4ee8a7c7d51516d0354ec5e45ca78dac5d6e2d0f
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-03 16:05:33 +0530

    Add FacebookAuthRequest DTO

--- src/main/java/com/aquapulse/backend/dto/FacebookAuthRequest.java
+++ src/main/java/com/aquapulse/backend/dto/FacebookAuthRequest.java
@@ -1,4 +1,11 @@
 package com.aquapulse.backend.dto;
 
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class FacebookAuthRequest {
+
+    @NotBlank
+    private String accessToken; // token received from Facebook JS SDK on frontend
 }
