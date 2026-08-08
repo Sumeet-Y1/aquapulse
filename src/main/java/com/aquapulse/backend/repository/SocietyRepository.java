4e2867ce73b7050529c76e5f4fe3a725ab9e89be
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-08 16:08:07 +0530

    Remove obsolete invite code methods from SocietyRepository

--- src/main/java/com/aquapulse/backend/repository/SocietyRepository.java
+++ src/main/java/com/aquapulse/backend/repository/SocietyRepository.java
@@ -2,9 +2,6 @@ package com.aquapulse.backend.repository;
 
 import com.aquapulse.backend.model.entity.Society;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
 
 public interface SocietyRepository extends JpaRepository<Society, Long> {
-    Optional<Society> findByInviteCode(String inviteCode);
-    boolean existsByInviteCode(String inviteCode);
 }
