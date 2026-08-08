921fd9e0b905fe11d3e9580e2cf5092192a0a198
Sumeet-Y1 <sumeety202@gmail.com>
2026-08-08 16:08:05 +0530

    Add InviteCodeResponse DTO

--- src/main/java/com/aquapulse/backend/dto/InviteCodeResponse.java
+++ src/main/java/com/aquapulse/backend/dto/InviteCodeResponse.java
@@ -1,4 +1,14 @@
 package com.aquapulse.backend.dto;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
 public class InviteCodeResponse {
+    private String code;
+    private LocalDateTime expiresAt;
+    private String type;
 }
